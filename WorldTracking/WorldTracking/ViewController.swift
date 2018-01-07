ea0693c58c4fbf918f9db821bfac578979e56d1c
Tkixi <tkixi@users.noreply.github.com>
2018-01-06 20:05:58 -0500

    Adding 3d object in camera view to world origin

--- WorldTracking/WorldTracking/ViewController.swift
+++ WorldTracking/WorldTracking/ViewController.swift
@@ -12,6 +12,13 @@ import ARKit
 
 class ViewController: UIViewController {
 
+    @IBAction func add(_ sender: Any) {
+        let node = SCNNode()
+        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+        node.position = SCNVector3(0,0,0)
+        self.sceneView.scene.rootNode.addChildNode(node)
+    }
     @IBOutlet weak var sceneView: ARSCNView!
     let configuration = ARWorldTrackingConfiguration()
     
